a3d5dbda2a0140d23498bd6bd08c293edc010496
Kayky <kaykyalmeida@hotmail.com.br>
2025-03-25 13:22:18 -0300

    correções do menu so dashboard

--- src/app/(app)/dashboard/layout.js
+++ src/app/(app)/dashboard/layout.js
@@ -4,7 +4,6 @@ import Link from "next/link";
 import styles from "./layout.module.css"
 import { usePathname } from "next/navigation";
 import { useEffect, useState } from "react";
-import { RxHamburgerMenu } from "react-icons/rx";
 
 export default function RootLayout({ children }) {
   
@@ -13,7 +12,6 @@ export default function RootLayout({ children }) {
   const [pedidos, setPedidos] = useState(false)
   const [avalicao, setAvalicao] = useState(false)
   const [estoque, setEstoque] = useState(false)
-  const [registros, setRegistros] = useState(false)
 
   const [menu, setMenu] = useState(false)
 
@@ -23,49 +21,33 @@ export default function RootLayout({ children }) {
       setPedidos(true)
       setAvalicao(false)
       setEstoque(false)
-      setRegistros(false)
     }
     if(pathName === "/dashboard/avaliacoes"){
       setPedidos(false)
       setAvalicao(true)
       setEstoque(false)
-      setRegistros(false)
     }
     if(pathName === "/dashboard/estoque"){
       setPedidos(false)
       setAvalicao(false)
       setEstoque(true)
-      setRegistros(false)
     }
-    if(pathName === "/dashboard/registros"){
-      setPedidos(false)
-      setAvalicao(false)
-      setEstoque(false)
-      setRegistros(true)
-    }
-    
   }, [pathName])
 
 
   return (
     <div className={styles.conteiner}>
       <div className={styles.conteinerDashboard}>
-        <div onClick={() => setMenu(!menu)} className={styles.menuButton}>
-          <RxHamburgerMenu strokeWidth={1} />
-        </div>
-        <nav className={`${styles.navigation} ${menu ? styles.navigationOn : styles.navigationOff}`}>
-          <ul>
-            <div style={pedidos ? {backgroundColor: "#222"} : {backgroundColor:"#111"}}>
+        <nav>
+          <ul className={styles.navigation}>
+            <div style={pedidos ? {backgroundColor: "#333"} : {backgroundColor:"#222"}}>
               <Link href={"/dashboard"}><li>Pedidos</li></Link>
             </div>
-            <div style={estoque ? {backgroundColor: "#222"} : {backgroundColor:"#111"}}>
+            <div style={estoque ? {backgroundColor: "#333"} : {backgroundColor:"#222"}}>
               <Link href={"/dashboard/estoque"}><li>Estoque</li></Link>
             </div>
-            <div style={avalicao ? {backgroundColor: "#222"} : {backgroundColor:"#111"}}>
+            <div style={avalicao ? {backgroundColor: "#333"} : {backgroundColor:"#222"}}>
               <Link href={"/dashboard/avaliacoes"}><li>Avaliações</li></Link>
-            </div>
-            <div style={registros ? {backgroundColor: "#222"} : {backgroundColor:"#111"}}>
-              <Link href={"/dashboard/registros"}><li>Registros</li></Link>
             </div>
           </ul>
         </nav>
